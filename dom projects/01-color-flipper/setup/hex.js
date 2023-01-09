7acaf424eb3075dfc6bc8933cf36859475322540
Manikantha_M <manikantha_m@Manikanthas-MacBook-Air.local>
2023-01-09 22:06:40 +0530

    color flipper completed

--- dom projects/01-color-flipper/setup/hex.js
+++ dom projects/01-color-flipper/setup/hex.js
@@ -1,1 +1,17 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn =document.getElementById('btn');
+const color =document.querySelector('.color');
+
+btn.addEventListener('click', function(){
+    let hexColor = '#';
+    for(let idx=0; idx<6; idx++){
+        hexColor += hex[getRandNum()]
+    }
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+})
+
+function getRandNum(){
+    return Math.floor(Math.random() * hex.length);
+}
