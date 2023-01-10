c2d9295af71f57f56c7e69d65422d0c75e6130ae
Manikantha_M <manikantha_m@Manikanthas-MacBook-Air.local>
2023-01-10 08:05:09 +0530

    counter

---
+++ dom projects/02-counter/setup/app.js
@@ -0,0 +1,21 @@
+// set initial count
+let count = 0;
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+console.log(btns)
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(event){
+        const styles = event.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count--;
+        }
+        else if(styles.contains('reset')){
+            count = 0;
+        }
+        else{
+            count++;
+        }
+        value.textContent = count;
+    })
+})
