386d73eedad0f8a646574c8096d8b664ed316cab
Manikantha_M <manikantha_m@Manikanthas-MacBook-Air.local>
2023-03-29 08:54:09 +0530

    object basics completed

--- objects/js/app1.js
+++ objects/js/app1.js
@@ -1,21 +1,25 @@
-// Objects Basics
-/* Objects are collections of key (property) - value pairs.
-The value can be string, number, boolean, array, function. 
+// Object Basics
+/* Object is a collection of key - value pairs.
+The value can be a string, number, boolean, array, object, method. 
 */
 
+const age = 35;
 const person = {
   name: 'john',
-  age: 25,
+  age,
   married: true,
   siblings: ['anna', 'susy'],
-  sayHello(name) {
-    console.log(`Hello I am ${name}`);
+  greet : function(){
+    return 'Hi'
   },
+  sayHello() {
+    return `Hello I am ${person.name}`;
+  },
+  'random-value':2323
 };
-console.log(person);
-console.log(person.name);
-person.age = 60;
-person.sayHello('Mani');
-// delete a key
-// delete person.siblings;
-console.log(delete person.siblings);
+console.log(person["random-value"]);
+console.log(person.greet());
+console.log(person.sayHello());
+const isDeleted = delete person.married;
+// Delete operation returns true or false.
+console.log(person, isDeleted)
