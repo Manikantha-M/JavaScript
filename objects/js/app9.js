c62dde6f0a5783f39903705f69aeaf757cc5b38e
Manikantha_M <manikantha_m@Manikanthas-MacBook-Air.local>
2023-04-06 08:18:58 +0530

    call() pass list of args

--- objects/js/app9.js
+++ objects/js/app9.js
@@ -1,14 +1,15 @@
 // call()
 /*
 The call() method calls the function with a given object.
+call(thisArg, arg1, arg2,...)
 */
 
-let greet = function(){
+let greet = function(city, country){
     console.log(this);
-    console.log(`I'm ${this.name} and my age is ${this.age}`);
+    console.log(`I'm ${this.name} and my age is ${this.age} and I live in ${city}, ${country}`);
 }
 let john = {
     name : 'john',
     age : 25
 }
-greet.call(john);
+greet.call(john, 'San Diego', 'US');
