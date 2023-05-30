d553d2a61d64c26ecb749e4709d3a6a35e82ad4f
Manikantha_M <manikantha_m@Manikanthas-MacBook-Air.local>
2023-05-30 07:54:08 +0530

    Global scope and Block scope are completed

---
+++ functions/js/app5.js
@@ -0,0 +1,7 @@
+/* Global Scope and Block Scope
+Global scope : 
+Global scoped variables or functions are accessible everywhere.
+
+Block scope : {}
+Block scoped variables or functions are accessible within the block {} only.
+*/
