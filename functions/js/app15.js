3cd4e2e65e698bb02ddcfd8396d2f5b5a395c9b4
Manikantha_M <manikantha_m@Manikanthas-MacBook-Air.local>
2023-06-13 07:25:55 +0530

    spread operator completed

--- functions/js/app15.js
+++ functions/js/app15.js
@@ -17,3 +17,17 @@ console.log(friends);
 const person = {name:'John',job:'dev'};
 const newPerson = {...person, city:'chicago',name:'Peter'};
 console.log(newPerson);
+
+const headings = document.querySelectorAll('h1');
+console.log(headings)
+const headingsArr = [...headings];
+console.log(headingsArr);
+
+const numbers = [23,45,66,88,2345];
+console.log(Math.max(...numbers));
+
+const info = ['john','sanders'];
+const sayHello = (firstName, lastName) => {
+    console.log(`${firstName} ${lastName}`);
+}
+sayHello(...info);
