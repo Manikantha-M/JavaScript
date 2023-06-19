f231043e1156c5aedc1ba8241c716215a7d20430
Manikantha_M <manikantha_m@Manikanthas-MacBook-Air.local>
2023-06-19 21:26:36 +0530

    find, findIndex, filter, every, some are completed

---
+++ functions/js/app18.js
@@ -0,0 +1,15 @@
+/* array.find(), array.findIndex(), array.every(), array.some() */
+
+const people = [
+    {name:'john'},
+    {name:'anna'},
+    {name:'peter'}
+]
+const grades = ['A','B','C','D'];
+
+console.log(people.filter(person => person.name === 'anna'));
+console.log(people.find(person => person.name === 'anna'));
+console.log(people.findIndex(person => person.name === 'peter'));
+
+console.log(grades.every(grade => grade !== 'E'));
+console.log(grades.some(grade => grade === 'C'))
