9a83c5174ab07f82f35a80fbdaa2dc9b6a857207
Manikantha_M <manikantha_m@Manikanthas-MacBook-Air.local>
2023-06-20 07:18:45 +0530

    Object keys values and entriesmethods are completed

---
+++ functions/js/app20.js
@@ -0,0 +1,10 @@
+/* Object.keys() ,Object.values(), Object.entries() */
+
+const person = {
+    name:'john',
+    age:25,
+    status:'student'
+};
+console.log(Object.keys(person));
+console.log(Object.values(person));
+console.log(Object.entries(person));
