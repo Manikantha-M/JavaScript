4df4933b02e00f285e5664cb404d75209e3cfc21
Manikantha_M <manikantha_m@Manikanthas-MacBook-Air.local>
2023-06-22 22:04:59 +0530

    array includes

--- functions/js/app22.js
+++ functions/js/app22.js
@@ -3,7 +3,7 @@ Array includes()  --> case sensitive
 */
 
 const personName = 'John';
-console.log(personName.toLocaleLowerCase().includes('jo'));
+console.log(personName.toLowerCase().includes('jo'));
 
 const groceries = ['milk','bread','meet'];
 console.log(groceries.includes('milk'));
