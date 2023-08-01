cfb5990b39e93147365b3eee7929a50708e7b752
Manikantha_M <manikantha_m@Manikanthas-MacBook-Air.local>
2023-08-01 07:48:29 +0530

    named export completed

---
+++ modules/js/app.js
@@ -0,0 +1,22 @@
+// Named export
+// import { random } from "../utils/data.js";
+// import { people } from "../utils/data.js";
+import { random, people  } from "../utils/data.js";
+
+
+// const container = document.querySelector('.container');
+// const btn = document.querySelector('.btn');
+
+// const showPeople = () => {
+//     const newPeople = people.map((person)=>{
+//         const {name, job} = person;
+//         return `<p>${name} <strong>${job}</strong></p>`
+//     }).join('');
+//     container.innerHTML = newPeople;
+// }
+
+// btn.addEventListener('click',()=>{
+//     showPeople()
+// })
+console.log(random);
+console.log(people)
---
+++ modules/utils/data.js
@@ -0,0 +1,16 @@
+// Named export
+// export const random = 'named export';
+
+// export const people = [
+//     {name:'john',job:'developer'},
+//     {name:'susan',job:'designer'},
+//     ]
+
+const random = 'named export';
+
+const people = [
+    {name:'john',job:'developer'},
+    {name:'susan',job:'designer'},
+]
+
+export {random, people};
