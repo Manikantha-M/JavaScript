2a1d6068190c8dd17c516e89f298c81297e0cbe4
Manikantha-M <manikanta.iiitnuz@gmail.com>
2023-08-02 21:18:14 +0530

    async javascript intro completed

---
+++ Async JavaScript/js/app1.js
@@ -0,0 +1,9 @@
+/* Async JavaScript 
+Asynchronous programming is a technique that enables your program to start a potentially long-running task and still be able to be responsive to other events while that task runs, rather than having to wait until that task has finished. Once the task has finished, your program is presented with the result.
+
+ex: setTimeout(()=>{},delay)
+The delay specified here is the minimum delay, the actual delay may be longer than intended.
+If delay is omitted, a value of 0 is used, meaning execute immediately, or more accurately, the next event cycle.
+
+*/
+
