e13d1a7fcbe1864b73955369f16c94b570d4aa10
Manikantha-M <manikanta.iiitnuz@gmail.com>
2023-08-04 07:32:59 +0530

    completed callback hell DOM example

--- Async JavaScript/js/app2.js
+++ Async JavaScript/js/app2.js
@@ -1,34 +1,54 @@
 /* Callback hell
+Nested callback functions are called callback hell.
+
 An example of callback hell
 
 Soup Recipe
 
-boil water 2min
+boil water 2sec
 chop carrots
 add carrots
-cook carrots 2min
+cook carrots 2sec
 chop onions
 add onions
-cook onions 2min
+cook onions 2sec
 
 */
 
-boilWater();
+// boilWater();
 
-function boilWater(){
-    console.log('started boiling water');
-    console.log('chop carrots');
-    setTimeout(()=>{
-        console.log('water boiled');
-        console.log('add carrots');
-        console.log('chop onions');
-        setTimeout(()=>{
-            console.log('cooked carrots');
-            console.log('add onions');
-            setTimeout(()=>{
-                console.log('cooked onions')
-                console.log('Soup ready')
-            },2000);
-        },2000)
-    },2000)
-}
+// function boilWater(){
+//     console.log('started boiling water');
+//     console.log('chop carrots');
+//     setTimeout(()=>{
+//         console.log('water boiled');
+//         console.log('add carrots');
+//         console.log('chop onions');
+//         setTimeout(()=>{
+//             console.log('cooked carrots');
+//             console.log('add onions');
+//             setTimeout(()=>{
+//                 console.log('cooked onions')
+//                 console.log('Soup ready')
+//             },2000);
+//         },2000)
+//     },2000)
+// }
+
+//Callback hell DOM example
+const heading1 = document.querySelector('.one') 
+const heading2 = document.querySelector('.two') 
+const heading3 = document.querySelector('.three')
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', ()=>{
+    setTimeout(() => {
+        heading1.style.color = 'red';
+        setTimeout(() => {
+            heading2.style.color = 'green';
+            setTimeout(() => {
+                heading3.style.color = 'blue';
+            }, 1000);
+        }, 1000);
+    }, 1000);
+})
