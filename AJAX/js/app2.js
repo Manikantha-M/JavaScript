bda4c16d3b66e0a10157959a17116065d66c04a2
Manikantha M <manikanta.iiitnuz@gmail.com>
2023-08-25 21:07:19 +0530

    get text data using fetch() method

--- AJAX/js/app2.js
+++ AJAX/js/app2.js
@@ -1,7 +1,7 @@
 /* fetch() 
 The Fetch API provides an interface for fetching resources (including across the network). It is a more powerful and flexible replacement for XMLHttpRequest.
 
-fetch() does not automatically converts the body to JSON, and should call the .json() method on the response.
+fetch() does not automatically convert the body to JSON, and should call the .json(), .text(), etc method on the response.
 */
 
 const url = './utils/people.json';
@@ -11,3 +11,22 @@ fetch(url).then((value)=>{
 },(reason)=>{
     console.log(reason)
 }).then(response => console.log(response))
+
+const url2 = './utils/sample.txt';
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+    fetch(url2)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.log('Fetch error:', error);
+        });
+});
