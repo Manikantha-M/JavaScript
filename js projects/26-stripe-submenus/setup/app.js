db77ffdfb0aba04222a6307a20706716c631d62e
Manikantha M <manikanta.iiitnuz@gmail.com>
2023-09-02 11:53:44 +0530

    completed stripe submenus

---
+++ js projects/26-stripe-submenus/setup/app.js
@@ -0,0 +1,71 @@
+import sublinks from "./data.js";
+const toggleBtn = document.querySelector(".toggle-btn");
+const closeBtn = document.querySelector(".close-btn")
+const sidebarWrapper = document.querySelector(".sidebar-wrapper")
+const sidebar = document.querySelector(".sidebar-links");
+const sidebarLinks = [...document.querySelectorAll(".link-btn")];
+const submenu = document.querySelector(".submenu");
+const hero = document.querySelector(".hero");
+const nav = document.querySelector(".nav");
+
+closeBtn.addEventListener("click",()=>{
+    sidebarWrapper.classList.remove("show");
+})
+toggleBtn.addEventListener("click",()=>{
+    sidebarWrapper.classList.add("show");
+})
+
+sidebar.innerHTML = sublinks.map((item)=>{
+    const {links,page} = item;
+    return `<article>
+    <h4>${page}</h4>
+    <div class="sidebar-sublinks">
+    ${links.map((link)=>{
+        return `<a href="${link.url}">
+        <i class="${link.icon}"></i>
+        ${link.label}</a>`
+    }).join("")}
+    </div>
+    </article>`
+}).join("");
+
+sidebarLinks.forEach(btn => {
+    btn.addEventListener("mouseover", function(e){
+        const text = e.currentTarget.textContent;
+        const tempBtn = e.currentTarget.getBoundingClientRect();
+        const center = (tempBtn.left + tempBtn.right)/2;
+        const bottom = tempBtn.bottom - 3;
+        const tempPage = sublinks.find(({page})=>page === text);
+        if(tempPage){
+            const {page,links} = tempPage;
+            submenu.classList.add("show");
+            submenu.style.left = `${center}px`;
+            submenu.style.top = `${bottom}px`;
+            // OPTIONAL
+            let columns = "col-2";
+            if(links.length == 3){
+                columns = `col-3`;
+            }
+            submenu.innerHTML = `
+            <section>
+            <h4>${page}</h4>
+            <div class="submenu-center ${columns}">
+            ${links.map((link)=> {return `
+            <a href="${link.url}">
+        <i class="${link.icon}"></i>
+        ${link.label}</a>
+            `}).join("")}
+            </div>
+            </section>
+            `
+        }
+    })
+});
+hero.addEventListener("mouseover", (e)=>{
+    submenu.classList.remove("show")
+})
+nav.addEventListener("mouseover", (e)=>{
+    if(!e.target.classList.contains("link-btn")){
+        submenu.classList.remove("show")
+    }
+})
